5d3a703d052a600997081ff196f5e623ea9740a6
rustem17 <40733085+rustem17@users.noreply.github.com>
2019-05-11 21:45:04 +0600

    Update and rename rustem_turtayev_assignment2.c to assignment2.c

--- assignment2.c
+++ assignment2.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
 #include <string.h>
 
-
-void readKey (char **keyArray) {                               // The function to read the key file using the pointer array created in main function
-
+void readKey (char **keyArray) {                               
+    // The function to read the key file using the pointer array created in main function
     char temp_keyArray[254];
     FILE *key;
     key = fopen("key.254", "r");
 
-    for (int i = 0; i < 254; ++i) {
+    for (int i = 0; i < 254; ++i)
         temp_keyArray[i] = fgetc(key);
-    }
 
-    *keyArray = temp_keyArray;                                 // Pointing the pointer to array that we've created and filled here
+    *keyArray = temp_keyArray;                                 
+    // Pointing the pointer to array that we've created and filled here
     fclose(key);
 }
 
@@ -20,21 +19,21 @@ void readKey (char **keyArray) {                               // The function t
 void encrypt (FILE *in, FILE *out, char keyArray[], char password[]) {
 
     int length = strlen(password);
-    int i = 0;                                                  // Iteration for password string
+    int i = 0;                                                  
+    // Iteration for password string
     while (1) {
         char inchar = fgetc(in);
-
-        if (inchar == EOF || inchar == '\0') break;             // To get rid of the last useless characters in string arrays
-
-        char index = (inchar + password[i]) % 254;              // Calculating the index for keyfile array
-
-        char write_char = keyArray[(unsigned char) index];      // Accessing the character from keyfile array by index that we calculated before
+        if (inchar == EOF || inchar == '\0') break;             
+        // To get rid of the last useless characters in string arrays
+        char index = (inchar + password[i]) % 254;              
+        // Calculating the index for keyfile array
+        char write_char = keyArray[(unsigned char) index];      
+        // Accessing the character from keyfile array by index that we calculated before
 
         putc(write_char, out);
-
         fflush(in);
-        fflush(out);                                            // This we need in order to read and write values properly
-
+        fflush(out);                                            
+        // This we need in order to read and write values properly
         i++;
         if (i == length) i = 0;
     }
@@ -42,16 +41,16 @@ void encrypt (FILE *in, FILE *out, char keyArray[], char password[]) {
 
 
 void decrypt (FILE *in, FILE *out, char keyArray[], char password[]) {
-
     int length = strlen(password);
     int i = 0;
     while (!feof(in)) {
         char inchar = fgetc(in);
         char index = 0;
 
-        if (inchar == EOF || inchar == '\0') break;             // To get rid of the last useless characters in string arrays
-
-        for (int j = 0; j < 254; j++) {                         // Finding out the character in keyfile array in order to get the index
+        if (inchar == EOF || inchar == '\0') break;             
+        // To get rid of the last useless characters in string arrays
+        for (int j = 0; j < 254; j++) {                         
+            // Finding out the character in keyfile array in order to get the index
             if (keyArray[j] == inchar) {
                 index = j;
                 break;
@@ -59,50 +58,48 @@ void decrypt (FILE *in, FILE *out, char keyArray[], char password[]) {
         }
 
         index -= password[i];
-        index %= 254;                                           // Operations for converting the index into character of original file
-
+        index %= 254;                                           
+        // Operations for converting the index into character of original file
         fputc(index, out);
-
         fflush(in);
-        fflush(out);                                            // This we need in order to write input values properly
-
+        fflush(out);                                            
+        // This we need in order to write input values properly
         i++;
-        if (i == length) i = 0;                                 // This piece we need for iteration of password string
+        if (i == length) i = 0;                                 
+        // This piece we need for iteration of password string
     }
 }
 
 
-void modPrint (char *printString) {                             // This function makes the life easier, because with it I do not care about the 'new line' chars in printf statements
-
+void modPrint (char *printString) {                             
+    // This function makes the life easier, because with it I do not care about the 'new line' chars in printf statements
     printf("%s\n", printString);
-
 }
 
 
 int main() {
-
     modPrint("WELCOME to the encryption service.");
-
     modPrint("ENTER your password");
     char password[256];
     scanf("%s", password);
+    
+    char array[254];                                             
+    // Creating the array for keyfile
+    char *keyArray;                                              
+    // Creating the pointer array for reading the key file in other functions
+    readKey(&keyArray);                                          
+    // Pointing to array thats being filled out in function readKey
 
-    char array[254];                                             // Creating the array for keyfile
-    char *keyArray;                                              // Creating the pointer array for reading the key file in other functions
-    readKey(&keyArray);                                          // Pointing to array thats being filled out in function readKey
-
-    for (int i = 0; i < 254; i++) {
-        array[i] = keyArray[i];                                  // Writing to local array in order to use in other functions as an array not pointer
-    }
+    for (int i = 0; i < 254; i++)
+        array[i] = keyArray[i];                                  
+        // Writing to local array in order to use in other functions as an array not pointer
 
     while (1) {
-
         modPrint("MENU: <e>ncode, <d>ecode, or <q>uit");
         char command[256];
         scanf("%s", command);
 
         if (command[0] == 'e') {
-
             modPrint("ENTER a file to encrypt");
             char in[256];
             scanf("%s", in);
@@ -128,10 +125,9 @@ int main() {
             }
 
             encrypt(fin, fout, array, password);
-
             fclose(fin);
             fclose(fout);
-
+            
         } else if (command[0] == 'd') {
 
             modPrint("ENTER a file to decrypt");
@@ -159,20 +155,14 @@ int main() {
             }
 
             decrypt(fin, fout, array, password);
-
             fclose(fin);
             fclose(fout);
-
+            
         } else if (command[0] == 'q') {
-
             modPrint("BYE!");
             break;
-
         } else {
-
             printf("UNRECOGNIZED %c\n", command[0]);
-
         }
     }
-
 }
